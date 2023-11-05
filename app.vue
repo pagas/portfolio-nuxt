c89a2ff5d235d389050f7db4390209a7ec3fa2f8
Paulius G <paulius@2simple.com>
2023-11-05 19:08:53 +0000

    set up the UI dependancies

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <!-- <NuxtWelcome /> -->
-    <h1>Hello Nuxt 3</h1>
+    <TheHeader />
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+      <NuxtPage />
+    </main>
+
   </div>
 </template>
