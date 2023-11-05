90a4a012dc567071e1e043332c0fd4c160064fad
Paulius G <paulius@2simple.com>
2023-11-05 21:03:17 +0000

    show posts list, callout component in MD

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,11 +1,21 @@
-<template>
-    <main>
-        <ContentDoc class="prose"/>
-    </main>
-</template>
+<script lang="ts" setup>
+const { path } = useRoute()
 
-<script setup lang="ts">
-
+const { data } = await useAsyncData(`content-${path}`, () => {
+    return queryContent()
+        .where({ _path: path })
+        .findOne()
+})
 </script>
 
-<style scoped></style>
+<template>
+    <template v-if="data != null">
+        <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
+        <div class="my-8">
+            <a v-for="tag in data.tags " :key="tag" :href="`/blog/tags/${tag}`"
+                class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
+                <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> {{ tag }}
+            </a>
+        </div>
+    </template>
+</template>
