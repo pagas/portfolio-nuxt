5d1a8f17bbe0d111cc96cbc683cb494c0d1961d5
Paulius G <paulius@2simple.com>
2023-11-05 19:59:04 +0000

    blog context and style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    'nuxt-icon'
-  ]
+    'nuxt-icon',
+    '@nuxt/content'
+  ],
+  content: {
+    highlight: {
+      theme: 'nord',
+      preload: ['ts', 'js', 'css', 'json', 'bash', 'vue']
+    }
+  }
 })
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <ContentDoc class="prose"/>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
