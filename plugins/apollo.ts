e3384d41b35f7fc98aa34d54264c89965c03bbd9
Paulius G <paulius@2simple.com>
2023-11-07 13:59:15 +0000

    don't need to convert to string

--- plugins/apollo.ts
+++ plugins/apollo.ts
@@ -7,6 +7,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook('apollo:auth', ({client, token}) => {
         // `client` can be used to differentiate logic on a per client basis.
         // apply apollo client token
-        token.value = String(githubToken);
+        token.value = githubToken;
     })
 })
