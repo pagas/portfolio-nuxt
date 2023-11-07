26dbf62f47c35bdb537c4deb816c269cf09a4fc9
Paulius G <paulius@2simple.com>
2023-11-07 13:57:39 +0000

    use npm instead of pnpm as cause off some typescript apollo nuxt config issues, get repos from personal github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,24 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxtjs/apollo'
   ],
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
   content: {
     highlight: {
       theme: 'nord',
       preload: ['ts', 'js', 'css', 'json', 'bash', 'vue']
     }
-  }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    },
+  },
 })
---
+++ plugins/apollo.ts
@@ -0,0 +1,12 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    const {githubToken} = useRuntimeConfig();
+
+    // access cookie for auth
+    // const cookie = useCookie('<cookie_name>')
+
+    nuxtApp.hook('apollo:auth', ({client, token}) => {
+        // `client` can be used to differentiate logic on a per client basis.
+        // apply apollo client token
+        token.value = String(githubToken);
+    })
+})
