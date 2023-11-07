a650466859bf00e5d125a5c4074f495c9a77dbff
Paulius G <paulius@2simple.com>
2023-11-07 18:23:47 +0000

    avoid prerender tags pages

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -12,10 +12,12 @@ const { data } = await useAsyncData(`content-${path}`, () => {
     <template v-if="data != null">
         <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
         <div class="my-8">
+          <client-only>
             <a v-for="tag in data.tags " :key="tag" :href="`/blog/tags/${tag}`"
                 class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
                 <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> {{ tag }}
             </a>
+          </client-only>
         </div>
     </template>
 </template>
