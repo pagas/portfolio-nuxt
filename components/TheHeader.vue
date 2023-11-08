2830d00ca55220369b437d7f2f069a411bdba995
Paulius G <paulius@2simple.com>
2023-11-08 16:06:34 +0000

    update content

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -27,9 +27,9 @@ const navigation = [
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-            <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            <img class="block h-8 w-auto lg:hidden" src="~/assets/images/gears.png"
               alt="Your Company" />
-            <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            <img class="hidden h-8 w-auto lg:block" src="~/assets/images/gears.png"
               alt="Your Company" />
           </div>
           <div class="hidden sm:ml-6 sm:block">
